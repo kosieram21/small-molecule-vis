af93154d6d20b2786ee47f507dae33b4a9c056a6
kosieram21 <mitchkos21@gmail.com>
2024-04-18 16:39:55 -0500

    code cleanup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { AppContextProvider } from './AppContext';
 import Toolbar from './Components/Toolbar';
 import MoleculeDrawingView from './Components/MoleculeDrawingView';
 import MoleculeSimulationView from './Components/MoleculeSimulationView';
-import AlertContainer from './Components/AlertContainer.js'
+import AlertContainer from './Components/AlertContainer.js';
 import './App.css';
 import Solution from './Object Model/Solution.js';
 
@@ -14,11 +14,11 @@ function App() {
             <AppContextProvider>
                   <div className='app'>
                         <Toolbar/>
-                        <AlertContainer/>
                         <div className='molecule-viewer-flex'>
                               <MoleculeDrawingView solution={solutionRef.current}/>
                               <MoleculeSimulationView solution={solutionRef.current}/>
                         </div>
+                        <AlertContainer/>
                   </div>
             </AppContextProvider>
   	);
